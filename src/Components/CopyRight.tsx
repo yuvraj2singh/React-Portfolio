17a591af517c95385a864c349da84785a5bce3d6
Yuvraj singh <yuvrajs2122005@gmail.com>
2025-02-15 12:10:50 +0530

    update CopyRight

--- src/Components/CopyRight.tsx
+++ src/Components/CopyRight.tsx
@@ -1,8 +1,8 @@
 const CopyRight = () => {
   return (
     <footer className="text-center py-4 text-gray-400">
-  &copy; {new Date().getFullYear()} YourWebsiteName. All rights reserved.
-</footer>
-  )
-}
-export default CopyRight
+      &copy; {new Date().getFullYear()} Yuvraj Singh. All rights reserved.
+    </footer>
+  );
+};
+export default CopyRight;
